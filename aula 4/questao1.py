ba6f3f77781063fe7836e23aee31ca8353e55d15
mathwqz <hs.pro.theuz@gmail.com>
2025-07-14 16:28:30 -0300

    up

--- aula 4/questao1.py
+++ aula 4/questao1.py
@@ -7,12 +7,13 @@ email = input("digite seu e-mail: ")
 # pedir a senha que ela quer usar
 senha = input("digite sua senha: ")
 
-# agora vamos ver se ela pode se cadastrar
+# regras:
 # tem que ter 13 anos ou mais
-# o email tem que ter o @
-# a senha tem que ter mais que 6 letras
+# o email tem que ter o @ e .com
+# o email deve estar todo em letras minúsculas
+# a senha tem que ter mais que 6 caracteres
 
-if idade >= 13 and "@" in email and len(senha) > 6:
+if idade >= 13 and "@" in email and ".com" in email and len(senha) > 6 and email == email.lower():
     print("cadastro aprovado!")
 else:
     print("dados invalidos")
